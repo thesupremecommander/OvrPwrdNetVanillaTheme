ed17a8584d9f23e21e68094b72abf123db9ff98f
thesupremecommander <thesupremecmdr@gmail.com>
2013-02-24 09:59:30 -0500

    Added Vanilla theme structure.
    Signed-off-by: thesupremecommander <thesupremecmdr@gmail.com>

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$ThemeInfo['OvrPwrdNet'] = array(
+   'Name' => 'OvrPwrdNet',
+   'Description' => "A theme designed to integrate into the OverPowered League website.",
+   'Version' => '1.0',
+   'Author' => "logs/thesupremecommander",
+   'AuthorEmail' => 'thesupremecmdr@gmail.com',
+   'AuthorUrl' => 'http://comp.ovrpwrd.net'
+);
